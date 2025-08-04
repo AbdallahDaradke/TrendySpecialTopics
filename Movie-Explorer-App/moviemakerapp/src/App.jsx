38b0f5ea866f05000e2b9e7ac7a06a6e000393d1
AbdallahDaradke <21110446@htu.edu.jo>
2025-08-04 19:22:11 +0300

    change title of the web

--- Movie-Explorer-App/moviemakerapp/src/App.jsx
+++ Movie-Explorer-App/moviemakerapp/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
+import MovieCard from "./components/MovieCard";
+import MovieDetails from "./pages/MovieDetails";
+import "./styles.css";
+import Navbar from "./components/Navbar";
+// import paths from "./components/Paths";
+import Paths from "./components/Paths";
+// import Navbar from "./components/Navbar";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Home />;
+  return (
+    <BrowserRouter>
+      {/* <Link to="/">Home</Link> */}
+
+      <Navbar />
+      <Paths />
+    </BrowserRouter>
+  );
 }
 
 export default App;
